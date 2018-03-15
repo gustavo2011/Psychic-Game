2d949c969d30480b57d072b6d7e0a9ed3e3a9d4f
gustavo2011 <gustavo.jauregui2011@gmail.com>
2018-03-14 22:06:30 -0700

    finished code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,46 @@ console.log(computerChoices);
 // variable to hold numbers
 var wins = 0;
 var losses = 0;
-var attempts = 9;
+var attemptsLeft = 9;
 var userGuessesPicked = [];
 var userGuesses = null;
 var letterToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log("wins: " + wins + " losses: " + losses + " attemptsLeft: " + attempts + " userGuessesPicked: " + userGuessesPicked + " computerGuess: " + letterToGuess);
+    console.log("wins: " + wins + " losses: " + losses + " attemptsLeft: " + attemptsLeft + " userGuessesPicked: " + userGuessesPicked + " computerGuess: " + letterToGuess);
 // function that runs whenever a key is pressed
 document.onkeyup = function(event){
-    console.log(event.key);
+    
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    if (letterToGuess === userGuess) {
+        wins++;
+        attemptsLeft = 9;
+        userGuessesPicked =[];
+        letterToGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+        console.log("wins: " + wins + " losses: " + losses + " attemptsLeft: " + attemptsLeft + " userGuessesPicked: " + userGuessesPicked + " computerGuess: " + letterToGuess);
+    }
+
+    if (userGuessesPicked.indexOf(userGuess) < 0 && computerChoices.indexOf(userGuess)>=0){
+        userGuessesPicked[userGuessesPicked.length]=userGuess;
+        attemptsLeft --;
+        console.log("wins: " + wins + " losses: " + losses + " attemptsLeft: " + attemptsLeft + " userGuessesPicked: " + userGuessesPicked + " computerGuess: " + letterToGuess);
+    }
+
+
+    if (attemptsLeft === 0) {
+        losses++;
+        attemptsLeft = 9;
+        userGuessesPicked = [];
+        letterToGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+        console.log("wins: " + wins + " losses: " + losses + " attemptsLeft: " + attemptsLeft + " userGuessesPicked: " + userGuessesPicked + " computerGuess: " + letterToGuess);
+    }
+    
+// displaying Html
+var html ="<h4>Wins: </h4> " + wins + "<h4>Losses: </h4>" + losses + "<h4>Attemps Left: <h4>" + attemptsLeft + "<h4> User Guesses Picked: </h4>" + userGuessesPicked;
+
+document.querySelector("#game").innerHTML = html;
 }
     
-    // actions when keys are pressed 
+    
 
 
 
